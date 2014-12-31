6190d648f938eea52604c69c19b4490990e81dd3
Shaheen Hyder <Shaheen Hyder@ShaheenHyder-PC>
2014-12-31 14:49:32 +0500

    tdd-part1 - Chapter 6 - part - 7

--- src/com/sibisoft/data/Franc.java
+++ src/com/sibisoft/data/Franc.java
@@ -1,7 +1,6 @@
 package com.sibisoft.data;
 
-public class Franc {
-	private Integer amount ;
+public class Franc extends Money{
 	
 	public  Franc(Integer amount){
 		this.amount = amount;
@@ -9,10 +8,5 @@ public class Franc {
 	
 	public Franc times(Integer multiplier){
 		return (new Franc(this.amount * multiplier));
-	}
-	
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return this.amount.intValue() == franc.amount.intValue();
 	}
 }
