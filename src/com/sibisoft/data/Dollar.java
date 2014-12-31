17e59f3ab92600600205310bc2cfaa81cdf01f80
Shaheen Hyder <Shaheen Hyder@ShaheenHyder-PC>
2014-12-31 11:44:21 +0500

    tdd-part1 - Chapter 1

--- src/com/sibisoft/data/Dollar.java
+++ src/com/sibisoft/data/Dollar.java
@@ -1,5 +1,21 @@
 package com.sibisoft.data;
 
 public class Dollar {
+	private Integer amount ;
+	
+	public  Dollar(Integer amount){
+		this.amount = amount;
+	}
 
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	
+	public void times(Integer multiplier){
+		this.amount *= multiplier;
+	}
 }
--- src/com/sibisoft/data/tests/DollarTest.java
+++ src/com/sibisoft/data/tests/DollarTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.sibisoft.data.Dollar;
+
 public class DollarTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDollar() {
+		assertEquals(new Integer(5), new Dollar(new Integer(5)).getAmount());
 	}
-
+	
+	@Test
+	public void testMultiplication(){
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(new Integer(10), five.getAmount());
+	}
 }
